9189239d9b9480dfdfbbc8f1df39127cf5b19334
roger.jiang <roger.jiang@afusion.com>
2019-09-01 14:18:50 +0800

    A fast-forward commit make development fast as possible

--- src/components/search591.jsx
+++ src/components/search591.jsx
@@ -17,13 +17,16 @@ class search591 extends Component {
 			filteredText: store.get('filteredText') || '',
 			houseData: [],
 			blackList: new Set(),
-			priceLow: 16,
+			protectList: new Set(),
+			priceLow: 17,
 			priceHigh: 18
 		}
 	}
+	
 	componentDidMount() {
 		const url = 'http://localhost:8888/api/search591'
 		this.fetchBlackList()
+		this.fetchProtectList()
 		this.fetchHouseData(url)
 	}
 
@@ -38,11 +41,55 @@ class search591 extends Component {
 		ax.get(url)
 			.then(response => {
 				this.setState({
-					blackList: new Set(response.data)
+					blackList: new Set(Object.keys(response.data))
 				})
 			})
 			.catch(err => {
 				console.log('err:', err)
+			})
+	}
+
+	fetchProtectList = () => {
+		const url = 'http://localhost:8888/api/search591/protect-list'
+		ax.get(url)
+			.then(response => {
+				this.setState({
+					protectList: new Set(Object.keys(response.data))
+				})
+			})
+			.catch(err => {
+				console.log('err:', err)
+			})
+	}
+
+	// TODO: add to protect list, remove from protect list
+	onClickAddProtectIcon = (e, id) => {
+		const url = 'http://localhost:8888/api/search591/protect-list'
+		e.stopPropagation()
+		ax.post(url, {
+			id: id
+		})
+			.then(response => {
+				console.log('response:', response)
+				this.fetchProtectList()
+			})
+			.catch(error => {
+				console.log('error:', error)
+			})
+	}
+
+	onClickDeleteProtectIcon = (e, id) => {
+		const url = 'http://localhost:8888/api/search591/protect-list/delete'
+		e.stopPropagation()
+		ax.post(url, {
+			id: id
+		})
+			.then(response => {
+				console.log('response:', response)
+				this.fetchProtectList()
+			})
+			.catch(error => {
+				console.log('error:', error)
 			})
 	}
 
@@ -91,9 +138,33 @@ class search591 extends Component {
 			})
 
 		const nextSet = new Set(this.state.blackList)
-		nextSet.add(id)
+		nextSet.add(id+'')
 		this.setState({
 			blackList: nextSet
+		})
+	}
+
+	onClickDeleteAllNotProtected = (e) => {
+		e.stopPropagation()
+		const { houseData,protectList } = this.state
+		const url = 'http://localhost:8888/api/search591/black-list/deleteAllNotProtected'
+		const allIds = houseData.map(item => item.id+'')
+		const idsWithoutProtected = allIds.filter(id => !protectList.has(id))
+		ax.post(url, {
+			ids: idsWithoutProtected
+		})
+			.then(response => {
+				console.log('response:', response)
+				this.fetchBlackList()
+			})
+			.catch(error => {
+				console.log('error:', error)
+			})
+
+		const nextBlackListSet = new Set(this.state.blackList)
+		idsWithoutProtected.forEach(id => nextBlackListSet.add(id+''))
+		this.setState({
+			blackList: nextBlackListSet
 		})
 	}
 
@@ -111,11 +182,11 @@ class search591 extends Component {
 			filteredText
 		} = this.state
 
-		const filteredTextArr = filteredText
-			.split(',')
-			.map(comp => comp.trim())
-			// not empty string and company name should at least two character
-			.filter(comp => comp && comp.length >= 2)
+		// const filteredTextArr = filteredText
+		// 	.split(',')
+		// 	.map(comp => comp.trim())
+		// 	// not empty string and company name should at least two character
+		// 	.filter(comp => comp && comp.length >= 2)
 
 		return (
 			<div>
@@ -156,6 +227,13 @@ class search591 extends Component {
 					>
 						送出
 					</Button>
+					<Button
+						style={{ marginRight: 15 }}
+						onClick={this.onClickDeleteAllNotProtected}
+					>
+						刪除全部 (除了Protected)
+					</Button>
+					
 					<div className="view-option">
 						<ThList
 							className={`${listView === 'detail' ? 'active' : ''}`}
@@ -170,7 +248,7 @@ class search591 extends Component {
 
 				<section
 					className={`item-house-wrap ${
-						this.state.listView === 'icon' ? 'sort-by-refresh' : ''
+						this.state.listView === 'icon' ? 'list-view-icon sort-by-refresh' : 'list-view-detail'
 					}`}
 				>
 					{this.state.houseData.length === 0 ? (
@@ -182,14 +260,14 @@ class search591 extends Component {
 						</ContentLoader>
 					) : (
 						this.state.houseData
-							.filter(
-								item =>
-									filteredTextArr.length === 0 ||
-									!filteredTextArr.some(filterComp =>
-										item.address.includes(filterComp)
-									)
-							)
-							.filter(item => !this.state.blackList.has(item.id))
+							// .filter(
+							// 	item =>
+							// 		filteredTextArr.length === 0 ||
+							// 		!filteredTextArr.some(filterComp =>
+							// 			item.address.includes(filterComp)
+							// 		)
+							// )
+							.filter(item => !this.state.blackList.has(item.id+''))
 							.sort(
 								(a, b) =>
 									this.state.sorting === 'price'
@@ -203,9 +281,12 @@ class search591 extends Component {
 								return (
 									<HouseItem
 										className={`item-house`}
-										key={`house-${idx}`}
+										key={`house-${item.id}`}
 										item={item}
+										isProtected={this.state.protectList.has(`${item.id}`)}
 										onClickDeleteIcon={this.onClickDeleteIcon}
+										onClickAddProtectIcon={this.onClickAddProtectIcon}
+										onClickDeleteProtectIcon={this.onClickDeleteProtectIcon}
 									/>
 								)
 							})
--- src/components/search591/HouseItem.js
+++ src/components/search591/HouseItem.js
@@ -1,48 +1,72 @@
-import React, { Component } from 'react'
-import moment from 'moment'
-import DeleteIcon from 'react-icons/lib/md/remove-circle'
+import React, { Component } from "react";
+import moment from "moment";
+import DeleteIcon from "react-icons/lib/md/remove-circle";
+import NoBookmark from "react-icons/lib/fa/bookmark-o";
+import Bookmarked from "react-icons/lib/fa/bookmark";
 
 class HouseItem extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-	onClickImageWrap = (e, url) => {
-		e.preventDefault()
-		window.open(url, '_blank')
-	}
+  onClickImageWrap = (e, url) => {
+    e.preventDefault();
+    window.open(url, "_blank");
+  };
 
-	render() {
-		const { item, onClickDeleteIcon } = this.props
-		return (
-			<div className={`item-house`}>
-				<a
-					onClick={e =>
-						this.onClickImageWrap(
-							e,
-							`https://rent.591.com.tw/rent-detail-${item.houseid}.html`
-						)
-					}
-				>
-					<img src={`${item.cover}`} className="house-coverImg" alt="" />
-					<DeleteIcon
-						className="delete-icon"
-						onClick={e => onClickDeleteIcon(e, item.id)}
-					/>
-				</a>
-				<span className="column-price">{item.price}</span>
-				<span className="column-address">{item.address}</span>
-				<span className="column-refreshtime">
-					{moment(item.refreshtime * 1000).fromNow()}
-				</span>
-				<span className="column-views">瀏覽次數:{item.browsenum}</span>
-				{/*
+  render() {
+    const {
+      item,
+      onClickDeleteIcon,
+      isProtected,
+      onClickAddProtectIcon,
+      onClickDeleteProtectIcon
+    } = this.props;
+
+    return (
+      <div className={`item-house`}>
+        <a
+          onClick={e => {
+            this.onClickImageWrap(
+              e,
+              `https://rent.591.com.tw/rent-detail-${item.houseid}.html`
+            );
+            onClickAddProtectIcon(e, item.id);
+          }}
+        >
+          {!isProtected && (
+            <NoBookmark
+              className="protect-item not-protected"
+              onClick={e => onClickAddProtectIcon(e, item.id)}
+            />
+          )}
+          {isProtected && (
+            <Bookmarked
+              className="protect-item protected"
+              onClick={e => onClickDeleteProtectIcon(e, item.id)}
+            />
+          )}
+          <img src={`${item.cover}`} className="house-coverImg" alt="" />
+          {!isProtected && (
+            <DeleteIcon
+              className="delete-icon"
+              onClick={e => onClickDeleteIcon(e, item.id)}
+            />
+          )}
+        </a>
+        <span className="column-price">{item.price}</span>
+        <span className="column-address">{item.address}</span>
+        <span className="column-refreshtime">
+          {moment(item.refreshtime * 1000).fromNow()}
+        </span>
+        <span className="column-views">瀏覽次數:{item.browsenum}</span>
+        {/*
         <span>價錢: {item.price}</span>
       */}
-			</div>
-		)
-	}
+      </div>
+    );
+  }
 }
 
-export default HouseItem
+export default HouseItem;
